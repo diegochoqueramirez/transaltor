eb79b165dc883900b5394f7df578ea37ccaa3d1f
Diego <diegochoqueramirez@gmail.com>
2021-09-10 10:19:26 -0400

    resolve stream

--- src/main/java/com/homework/translator/service/TraductorService.java
+++ src/main/java/com/homework/translator/service/TraductorService.java
@@ -6,22 +6,51 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TraductorService {
-
-    public String revert(String song) {
-        String[] arreglo = song.split("\r\n\r\n");
-        String result = "";
-        for (int i = arreglo.length - 1 ; i >= 0; i--) {
-                result += arreglo[i] + "\r\n\r\n";
-        }
-        return result;
-    }
 
     public String getStringFromInputStream(InputStream input) throws IOException {
         StringWriter writer = new StringWriter();
         IOUtils.copy(input, writer, "UTF-8");
         return writer.toString();
+    }
+
+    public String revert(String song) {
+        String[] words = song.split("\r\n\r\n");
+        String result = "";
+        for (int i = words.length - 1 ; i >= 0; i--) {
+                result += words[i] + "\r\n\r\n";
+        }
+        return result;
+    }
+
+    public String count(String song) {
+        Map<String, Integer> ocurrences = new HashMap<String,Integer>();
+        String[] stanzas = song.split("\r\n\r\n");
+        String[] words = song.split("[\\s,]+");
+
+        for (String word : words) {
+            if (ocurrences.containsKey(word)) {
+                ocurrences.put(word, ocurrences.get(word)+1);
+            } else {
+                ocurrences.put(word, 1);
+            }
+        }
+
+//        for (Map.Entry<String, Integer> entry : ocurrences.entrySet()) {
+//            System.out.println(entry.getKey() + " - " + entry.getValue());
+//        }
+
+        Integer value = ocurrences.entrySet().stream().max((entry1, entry2) -> entry1.getValue() - entry2.getValue()).get().getValue();
+        String key = ocurrences.entrySet().stream().max((entry1, entry2) -> entry1.getValue() - entry2.getValue()).get().getKey();
+        return "El numero de estrofas es "+ stanzas.length + "\n" + "La palabra que mas se repite es " + key + " con " + value + " veces";
+    }
+
+    public String replace(String song) {
+        String result = song.replace("beggin'", "you");
+        return result;
     }
 }
--- src/test/java/com/homework/translator/service/TraductorServiceTest.java
+++ src/test/java/com/homework/translator/service/TraductorServiceTest.java
@@ -17,13 +17,40 @@ public class TraductorServiceTest {
 
     @Test
     void revertSong() throws IOException {
-        String inputFileName = "src/main/java/com/homework/translator/Original.txt";
-        String outputFileName = "src/main/java/com/homework/translator/estrofasEnOrdenInverso.txt";
-        File outputFile = new File(outputFileName);
-        InputStream in = new FileInputStream(inputFileName);
-        OutputStream out = new FileOutputStream(outputFile);
+        String original = "src/main/java/com/homework/translator/Original.txt";
+        String estrofasEnOrdenamiento = "src/main/java/com/homework/translator/estrofasEnOrdenInverso.txt";
 
-        String inputFileContent = traductorService.getStringFromInputStream(new FileInputStream(inputFileName));
-        StreamUtils.copy(traductorService.revert(inputFileContent), StandardCharsets.UTF_8,out);
+        File order = new File(estrofasEnOrdenamiento);
+        OutputStream outStanzas = new FileOutputStream(order);
+
+        String originalFile = traductorService.getStringFromInputStream(new FileInputStream(original));
+        StreamUtils.copy(traductorService.revert(originalFile), StandardCharsets.UTF_8, outStanzas);
     }
+
+    @Test
+    void saveStatistics() throws IOException {
+        String estrofasEnOrdenamiento = "src/main/java/com/homework/translator/estrofasEnOrdenInverso.txt";
+        String statistics = "src/main/java/com/homework/translator/statistics.txt";
+
+        File count = new File(statistics);
+        OutputStream outStatistic = new FileOutputStream(count);
+
+        String estrofasOrdenadas = traductorService.getStringFromInputStream(new FileInputStream(estrofasEnOrdenamiento));
+        StreamUtils.copy(traductorService.count(estrofasOrdenadas), StandardCharsets.UTF_8, outStatistic);
+
+    }
+
+    @Test
+    void saveOutputFile() throws IOException {
+        String estrofasEnOrdenamiento = "src/main/java/com/homework/translator/estrofasEnOrdenInverso.txt";
+        String finalOutput = "src/main/java/com/homework/translator/finaloutput.txt";
+
+        File output = new File(finalOutput);
+        OutputStream outFinal = new FileOutputStream(output);
+
+        String estrofasOrdenadas = traductorService.getStringFromInputStream(new FileInputStream(estrofasEnOrdenamiento));
+        StreamUtils.copy(traductorService.replace(estrofasOrdenadas), StandardCharsets.UTF_8, outFinal);
+    }
+
+
 }
